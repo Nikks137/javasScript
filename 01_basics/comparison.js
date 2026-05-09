be3b953a9943bda38c4161f347e42d4458c2e961
Nikks137 <nikharskumr8893@gmail.com>
2026-05-09 14:06:52 +0000

    comparison done

---
+++ 01_basics/comparison.js
@@ -0,0 +1,28 @@
+// console.log(2 > 3);//false
+// console.log(2 >= 3);//false
+// console.log(2 < 3);//true
+// console.log(2 <= 3);//true
+// console.log(2 == 3);//true
+
+
+console.log("2" > 1);//true
+console.log("02" > 1);//true
+console.log(2 > "1");//true
+console.log(2 < "1");//false
+console.log("2" < 1);//false
+
+// console.log(null > 0);//false
+// console.log(null >= 0);//true
+// console.log(null < 0);//false
+// console.log(null <= 0);//true
+// console.log(null == 0);//false
+// console.log(null != 0);//true
+
+
+// console.log(undefined > 0);//false
+// console.log(undefined >= 0);//false
+// console.log(undefined < 0);//false
+// console.log(undefined <= 0);//false
+// console.log(undefined == 0);//false
+// console.log(undefined != 0);//true
+
