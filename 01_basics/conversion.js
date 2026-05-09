4b6b42d36298d2cc8890192e7dada92b31ab7e60
Nikks137 <nikharskumr8893@gmail.com>
2026-05-09 08:28:40 +0000

    conversion

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let student_Id = 33
+
+let studentNumber = String(student_Id);
+
+let temp = null;
+let initialTemp = String(temp);
+
+let name = "nikhil"
+let num = Number(name);
+
+// console.log(student_Id)
+// console.log(studentNumber)
+
+// console.log(typeof student_Id)//number
+// console.log(typeof studentNumber)//string
+
+// console.log(temp)
+// console.log(initialTemp)
+
+// console.log(typeof temp)//object
+// console.log(typeof initialTemp)//string
+
+console.log(name)//nikhil
+console.log(num)//NaN
+
+console.log(typeof name)
+console.log(typeof num)
