12b1ca0550f23dd14d58779585c9b626e324df5a
Nikks137 <nikharskumr8893@gmail.com>
2026-05-09 08:13:11 +0000

    variables and types

---
+++ 01_basics/letConstVar.js
@@ -0,0 +1,18 @@
+const score = 74
+let studentName = "Nikhil"
+var stduntCity = "gorakhpur"//not in use in noe days because of block scope and functional scope.
+let studentNumber;
+let student = null
+console.log(score);
+console.table([ score, studentName, stduntCity, studentNumber, student])
+
+console.log(typeof student);
+
+//the type of null data type is object
+
+
+//outputs on comments
+console.log(typeof studentName);//string
+console.log(typeof studentNumber);//undefined
+console.log(typeof score);//number
+console.log(typeof student);//object
