f0e84d21b85834562b21bdc004a88a7751fe2ce0
Nikks137 <nikharskumr8893@gmail.com>
2026-05-10 07:36:49 +0000

    string read on mdn

---
+++ 01_basics/string.js
@@ -0,0 +1,19 @@
+let name = "shagun"
+let age = 20
+
+//console.log(`my name is ${name} and my age is ${age}`);
+
+let naam = new String('nikhil');
+// const gameName = new String('hitesh-hc-com')
+//console.log(`my name is ${naam}`);
+
+// console.log(naam[0]);//n
+// console.log(naam.length);//6
+// console.log(naam.toUpperCase());//NIKHIL
+
+
+// console.log(naam.substring(0, 4));//it only take value from 0
+// console.log(naam.slice(-9, 3));//it also contain - values
+
+console.log(naam.charAt(4));
+console.log(naam.indexOf('i'));
